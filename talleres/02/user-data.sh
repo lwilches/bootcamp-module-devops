4c9431ef263f67c608a16a508e31ae2d79671d38
EC2 Default User <ec2-user@ip-172-31-60-33.ec2.internal>
2020-07-09 01:09:42 +0000

    ajuste userdata

--- talleres/02/user-data.sh
+++ talleres/02/user-data.sh
@@ -1,9 +1,13 @@
 #!/bin/bash -xe
 
+exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+
+yum update  -y 
 yum install -y python37
+yum  install -y git 
 curl -O https://bootstrap.pypa.io/get-pip.py
 python3 get-pip.py
-git clone https://github.com/eloyvega/bootcamp-devops.git
+git clone https://github.com/lwilches/bootcamp-module-devops.git
 cd ~/bootcamp-devops/backend/
 pip install -r requirements.txt
 nohup gunicorn -w 3 -b 0.0.0.0:8000 app:app &
