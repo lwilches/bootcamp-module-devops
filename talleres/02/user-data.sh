e19ece9b68428137388c408a0b66c53b460e5d81
Leonardo Wilches <lwilches@gmail.com>
2020-07-09 01:46:32 +0000

    se actualiza user data

--- talleres/02/user-data.sh
+++ talleres/02/user-data.sh
@@ -1,13 +1,14 @@
 #!/bin/bash -xe
 
-exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+exec > >(tee /var/log/cloud-init-output.log|logger -t user-data -s 2>/dev/console) 2>&1
 
 yum update  -y 
 yum install -y python37
 yum  install -y git 
 curl -O https://bootstrap.pypa.io/get-pip.py
 python3 get-pip.py
+cd ~
 git clone https://github.com/lwilches/bootcamp-module-devops.git
-cd ~/bootcamp-devops/backend/
+cd /bootcamp-devops/backend/
 pip install -r requirements.txt
 nohup gunicorn -w 3 -b 0.0.0.0:8000 app:app &
